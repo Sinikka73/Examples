fb40142aa215c1ceddc99a4e7a1da5b6d6329b0f
olga <bend_sinister@mail.ru>
2022-09-21 12:16:02 +0300

    Ex10

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = { 11, 21, 3, 4, 18, 65, 76, 37, 18 };
+int n = array.Length;
+int find = 18;
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+       Console.WriteLine(index);
+       break;
+    }
+    index++;
+}
